230fef3101ccec10303d9b09d7e55351f8dc98d8
Peter <57006688+TheOfficialPeter@users.noreply.github.com>
2023-11-19 14:51:45 +0200

    added more stuff
    Added the UI for attaching games to the application

--- events.js
+++ events.js
@@ -1,6 +1,12 @@
-window.onload = function() {
+function refreshWindow() {
+    // refresh window to initial state
+    window.location.reload();
+}
+
+window.onload = async function () {
     // game profile menu
     var state = 1;
+    const blur = document.querySelector('.blur')
     const finalConnectButton = document.querySelector('.formPopup #button');
     const connectButton = document.getElementsByClassName('infoButton')[0];
     const formPopup = document.getElementsByClassName('formPopup')[0];
@@ -8,14 +14,164 @@ window.onload = function() {
     const removeButton = document.getElementsByClassName('removeButton')[0];
 
     if (finalConnectButton) {
-        finalConnectButton.onclick = function() {
+        finalConnectButton.onclick = async function () {
+            const loading = document.querySelector('.formPopup #loading');
+            const button = document.querySelector('.formPopup #button');
             const formTitle = document.querySelector('.formPopup #title');
-            const formInput = document.querySelector('.formPopup #title');
+            const labels = document.querySelectorAll('.formPopup #inputLabel');
+            const robloxInput = document.querySelector('.Roblox');
+            const steamInput = document.querySelector('.Steam');
+
+            const steamID = steamInput.value;
+            const robloxID = robloxInput.value;
+
+            if (steamID != "") {
+                // find steam game stats using id
+                const response = await fetch(`https://vginsights.com/api/v1/game/${steamID}/quick-stats`);
+                const status = response.status;
+                
+                if (status == 200) {
+                    formTitle.innerText = "Connecting..."
+                    robloxInput.remove();
+                    steamInput.remove();
+                    button.remove();
+
+                    labels.forEach(function (e) {
+                        e.remove();
+                    })
+
+                    loading.style.display = "block";
+
+                    setTimeout(function () {
+                        loading.style.display = "none";
+                        const loadingClone = loading.cloneNode();
+                        loading.remove();
+
+                        loadingClone.style.backgroundImage = "url(assets/Ok.svg)";
+                        loadingClone.style.display = "block";
+                        loadingClone.style.animation = "none";
+                        formTitle.innerText = "Game added";
+                        formPopup.appendChild(loadingClone);
+
+                        // remove popup
+                        setTimeout(function () {
+                            blur.remove()
+                        }, 500)
+                    }, 1000)
+                }
+                else {
+                    // game not found
+                    formTitle.innerText = "Connecting..."
+                    robloxInput.remove();
+                    steamInput.remove();
+                    button.remove();
+
+                    labels.forEach(function (e) {
+                        e.remove();
+                    })
+
+                    loading.style.display = "block";
+
+                    setTimeout(function () {
+                        loading.style.display = "none";
+                        formTitle.innerText = "Game not found"
+                        robloxInput.remove();
+                        steamInput.remove();
+                        button.remove();
+
+                        const icon = loading.cloneNode();
+                        icon.style.backgroundImage = "url(./assets/404.svg)";
+                        icon.style.display = "block";
+                        icon.style.animation = "none";
+
+                        labels.forEach(function (e) {
+                            e.remove();
+                        })
+
+                        formPopup.appendChild(icon);
+
+                        setTimeout(function () {
+                            refreshWindow();
+                        }, 1000)
+                    }, 1000)
+                }
+            }
+            else if (robloxID != "") {
+                // find roblox game stats using id
+                const response = await fetch(`https://www.roblox.com/games/${robloxID}`);
+                const status = response.status;
+
+                if (status == 200) {
+                    formTitle.innerText = "Connecting..."
+                    robloxInput.remove();
+                    steamInput.remove();
+                    button.remove();
+
+                    labels.forEach(function (e) {
+                        e.remove();
+                    })
+
+                    loading.style.display = "block";
+
+                    setTimeout(function () {
+                        loading.style.display = "none";
+                        const loadingClone = loading.cloneNode();
+                        loading.remove();
+
+                        loadingClone.style.backgroundImage = "url(assets/Ok.svg)";
+                        loadingClone.style.display = "block";
+                        loadingClone.style.animation = "none";
+                        formTitle.innerText = "Game added";
+                        formPopup.appendChild(loadingClone);
+
+                        // remove popup
+                        setTimeout(function () {
+                            blur.remove()
+                        }, 500)
+                    }, 1000)
+                }
+                else {
+                    // game not found
+                    formTitle.innerText = "Connecting..."
+                    robloxInput.remove();
+                    steamInput.remove();
+                    button.remove();
+
+                    labels.forEach(function (e) {
+                        e.remove();
+                    })
+
+                    loading.style.display = "block";
+
+                    setTimeout(function () {
+                        loading.style.display = "none";
+                        formTitle.innerText = "Game not found"
+                        robloxInput.remove();
+                        steamInput.remove();
+                        button.remove();
+
+                        const icon = loading.cloneNode();
+                        icon.style.backgroundImage = "url(./assets/404.svg)";
+                        icon.style.display = "block";
+                        icon.style.animation = "none";
+
+                        labels.forEach(function (e) {
+                            e.remove();
+                        })
+
+                        formPopup.appendChild(icon);
+
+                        setTimeout(function () {
+                            refreshWindow();
+                        }, 1000)
+                    }, 1000)
+                }
+            }
         }
     }
 
     if (connectButton) {
-        connectButton.onclick = function() {
+        connectButton.onclick = function () {
             const infoIcon = document.getElementsByClassName('info')[0];
             const infoTitle = document.getElementsByClassName('infoTitle')[0];
 
@@ -29,21 +185,20 @@ window.onload = function() {
     }
 
     if (profileIcon) {
-        profileIcon.onclick = function() {
+        profileIcon.onclick = function () {
             if (state == 1) {
-                removeButton.style.display = "flex"; 
-                setTimeout(function(){
-                    removeButton.style.opacity = "1"; 
-                },10)
+                removeButton.style.display = "flex";
+                setTimeout(function () {
+                    removeButton.style.opacity = "1";
+                }, 10)
 
                 state = 0;
             }
-            else
-            {
-                removeButton.style.opacity = "0"; 
-                setTimeout(function(){
-                    removeButton.style.display = "none"; 
-                },200)
+            else {
+                removeButton.style.opacity = "0";
+                setTimeout(function () {
+                    removeButton.style.display = "none";
+                }, 200)
 
                 state = 1;
             }
